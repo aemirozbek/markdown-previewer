b5c9e2d0012cbea37f92aeaa1380f7a4114c494e
agubucu <aemirozbek@gmail.com>
2023-07-01 18:57:44 +0300

    Styling and responsiveness

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
 import { marked } from "marked";
 import { useState } from "react";
 import { defaultContent } from "./defaulContent";
-import * as DOMPurify from 'dompurify';
+import DOMPurify from "dompurify";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   marked.use({
     breaks: true,
   });
-  
-  const [content, setContent] = useState(defaultContent)
+
+  const [content, setContent] = useState(defaultContent);
   let clean = DOMPurify.sanitize(content);
 
-  
   function handleChange(event) {
-    setContent(event.target.value)
+    setContent(event.target.value);
   }
   return (
-    <div className="App">
-      <textarea id="editor" onChange={handleChange} value={clean}></textarea>
-      <div
-        id="preview"
-        dangerouslySetInnerHTML={{
-          __html: marked(clean),
-        }}
-      ></div>
+    <div className="App container">
+      <div>
+        <div id="editor-wrapper">
+          <div className="wrapper-header">Editor</div>
+          <textarea
+            id="editor"
+            onChange={handleChange}
+            value={clean}
+          ></textarea>
+        </div>
+        <div id="preview-wrapper">
+          <div className="wrapper-header">Preview</div>
+          <div
+            id="preview"
+            dangerouslySetInnerHTML={{
+              __html: marked(clean),
+            }}
+          ></div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/defaulContent.js
+++ src/defaulContent.js
@@ -1,27 +1,57 @@
-export const defaultContent = `# This is a Header (H1 size)
+export const defaultContent = `# **React Markdown Previewer**
 
-  ## This is a Sub Header (H2 size)
-  
-  [Link to GitHub](https://github.com)
-  
-  Inline code: \`console.log("Hello, World!");\`
-  
-  Code block:
-  
-  \`\`\`javascript
-  function greet(name) {
-    console.log("Hello, " + name + "!");
-  }
-  
-  greet("Alice");
-  \`\`\`
-  
-  - List item 1
-  - List item 2
-  - List item 3
-  
-  > This is a blockquote.
-  
-  ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
-  
-  **This is bolded text.**`
+<br>
+
+# This is a Header (H1 size)
+
+## This is a Sub Header (H2 size)
+
+[Link to GitHub](https://github.com)
+
+Inline code: \`console.log("Hello, World!");\`
+
+Code block:
+
+\`\`\`javascript
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+
+greet("Alice");
+\`\`\`
+
+- List item 1
+- List item 2
+- List item 3
+
+> This is a blockquote.
+
+an image:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+
+**This is bolded text.**
+
+Emoji:
+
+😄🎉🚀
+Rainbow Text:
+
+<span style="color: red;">R</span>
+<span style="color: orange;">a</span>
+<span style="color: yellow;">i</span>
+<span style="color: green;">n</span>
+<span style="color: blue;">b</span>
+<span style="color: indigo;">o</span>
+<span style="color: violet;">w</span>
+<span style="color: red;">T</span>
+<span style="color: orange;">e</span>
+<span style="color: yellow;">x</span>
+<span style="color: green;">t</span>
+<span style="color: blue;">!</span>
+
+a Table:
+Some Header | Another Header | Some Other Header
+------------ | ------------- | -------------
+Some content | Some more content | even more content
+even even more | even x 3 more | even x 4`
